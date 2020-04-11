e8b0bad36b14a6dd357499ae3f6beacadd0b4802
malric.philippe@gmail.com <malric.philippe@gmail.com>
2020-04-11 14:20:20 -0400

    Ajout d'un component test

---
+++ resources/assets/js/app.js
@@ -0,0 +1,15 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('test', require('./components/Test.vue').default);
+
+const app = new Vue({
+  el: '#app'
+});
